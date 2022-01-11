0596a5d0d42c9cee5ba23e78fe48856fda1d713b
Khaled Nassar <kmhnassar@gmail.com>
2022-01-11 15:08:52 +0100

    Add tests for highlight transformation

---
+++ tests/rewriting/test_highlight.py
@@ -0,0 +1,26 @@
+from obyde.rewriting.highlight import ObsidianHighlightRewritingTransformer
+
+def test_highlight_does_not_modify_preformatted_blocks():
+    preformatted = "if a == b and b == c:\n\tpass"
+    highlighter = ObsidianHighlightRewritingTransformer()
+    highlighted = highlighter.transform_preformatted_block(preformatted)
+    assert highlighted is None
+
+def test_highlight_does_not_modify_metadata_blocks():
+    metadata = '---\ndate: 2021-08-21\ntitle: "== title =="'
+    highlighter = ObsidianHighlightRewritingTransformer()
+    highlighted = highlighter.transform_preformatted_block(metadata)
+    assert highlighted is None
+
+def test_highlight_does_not_modify_content_blocks_with_no_highlights():
+    content = "This is a normal content block with no highlighted text."
+    highlighter = ObsidianHighlightRewritingTransformer()
+    highlighted = highlighter.transform_normal_block(content)
+    assert highlighted is None
+
+def test_highlight_markup_content_block():
+    content = "This is a normal content block with ==highlighted text==."
+    excpected = "This is a normal content block with <mark>highlighted text</mark>."
+    highlighter = ObsidianHighlightRewritingTransformer()
+    highlighted = highlighter.transform_normal_block(content)
+    assert highlighted == excpected
