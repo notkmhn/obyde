769f49bacb3381f197b3f46dd14cc51ac9a5e499
Khaled Nassar <kmhnassar@gmail.com>
2022-01-11 15:23:05 +0100

    Implement Obsidian highlighting transformation

--- obyde/rewriting/highlight.py
+++ obyde/rewriting/highlight.py
@@ -1,11 +1,15 @@
+import re
 from typing import Optional
 
 from .model import RewritingTransformer
 
 
 class ObsidianHighlightRewritingTransformer(RewritingTransformer):
+    HIGHLIGHT_REGEX = re.compile(r"==(.*)==", flags=re.DOTALL)
+
     def transform_normal_block(self, block: str) -> Optional[str]:
-        return None
+        transformed, count = self.HIGHLIGHT_REGEX.subn(r"<mark>\1</mark>", block)
+        return transformed if count != 0 else None
 
     def transform_preformatted_block(self, block: str) -> Optional[str]:
         # Does not modify preformatted blocks
